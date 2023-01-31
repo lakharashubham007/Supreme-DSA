a297f01af75b4fb0539e5871b5cf92b96b92296d
lakharashubham007 <lakharashubham623@gmail.com>
2023-01-31 08:14:29 +0530

    session2

---
+++ Session3/ClassWork/pattern6.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    for(int row=0; row<n; row++){
+        for(int col=0; col<row+1; col++){
+            cout<<col+1;
+        }
+        cout<<endl;
+    }
+}
+
+/* Output 
+
+1
+12
+123
+1234
+12345
+ 
+  */
