a6ed0e2a7e74c891a69eb78d5568f532efa5ddc2
lakharashubham007 <lakharashubham623@gmail.com>
2023-05-22 19:58:45 +0530

    DSA

---
+++ YTSolution_Supreme_batch/Arrays/dummy.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int arr[] = {0,1,0,1,1,0,0,1,0,1,};
+    int size = 10;
+
+    int countZero = 0;
+    int countOne = 0;
+
+    for(int i=0; i<size; i++){
+        if(arr[i] == 0){
+            countZero++;
+        }
+        if(arr[i] == 1){
+            countOne++;
+        }
+    }
+    
+    cout<<"No. of zero: "<<countZero<<endl;
+    cout<<"No. of one: "<<countOne<<endl;
+    return 0;
+}
